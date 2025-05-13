30a6fe73c0052cab59f1fc51e7c6c1033be72c98
Koshkovka <kotyonok.evseev@yandex.ru>
2025-05-13 19:19:00 +0300

    Created main window

--- src/docx/templates/DocxTemplates.java
+++ src/docx/templates/DocxTemplates.java
@@ -4,6 +4,8 @@
  */
 package docx.templates;
 
+import docx.templates.gui.MainWindow;
+
 /**
  *
  * @author kotyo
@@ -14,7 +16,9 @@ public class DocxTemplates {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MainWindow window = new MainWindow();
+        window.show();
+        System.out.println("hello");
     }
     
 }
