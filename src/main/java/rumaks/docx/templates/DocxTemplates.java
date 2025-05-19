d326dcd0f1b041d5b5bd6c5955c314295da2e7b3
Koshkovka <kotyonok.evseev@yandex.ru>
2025-05-19 18:53:42 +0300

    Создал проектик

---
+++ src/main/java/rumaks/docx/templates/DocxTemplates.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package rumaks.docx.templates;
+
+import rumaks.docx.templates.gui.mainWindow;
+
+/**
+ *
+ * @author kotyo
+ */
+public class DocxTemplates {
+
+    public static void main(String[] args) {
+        mainWindow window = new mainWindow();
+        window.show();
+    }
+}
